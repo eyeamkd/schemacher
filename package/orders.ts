31a373f3eadb087f09eec408d75373982a7cb84c
Kunal Dubey <kunal.dubey@dazn.com>
2024-06-05 19:12:38 +0530

    final check

--- package/orders.ts
+++
@@ -1,5 +0,0 @@
-export interface orders {
-  id: string;
-  price: number;
-  commodityNumber: number;
-}
--- package/user.ts
+++
@@ -1,7 +0,0 @@
-export interface user {
-  id: string;
-  name: string;
-  email: string;
-  address: string;
-  phone: string;
-}
