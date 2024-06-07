26821a7cf555ecc96f1f2b7fe7e5b4d59d38968b
TypeMaster <kunaldubey2297@gmail.com>
2024-06-07 15:43:47 +0000

    Generated TypeScript client files

---
+++ package/orders.ts
@@ -0,0 +1,4 @@
+export interface orders {
+  id: string;
+  price: number;
+}
---
+++ package/user.ts
@@ -0,0 +1,7 @@
+export interface user {
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}
